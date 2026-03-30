e8e8a94774384e84c18f3d6c15b6cb28a2c90ad9
Fanantenana Ny Aina Rakotoarison <fananrak@c1r1s5.42antananarivo.mg>
2026-03-30 13:57:22 +0300

    feat: init algo structure

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 10:04:42 by fananrak          #+#    #+#             */
-/*   Updated: 2026/03/30 12:50:14 by fananrak         ###   ########.fr       */
+/*   Updated: 2026/03/30 13:56:36 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 
 char    *get_next_line(int  fd)
 {
-    if (fd < 0)
-    {
-        return (0);
-    }
-    
+    static char *text;
+    char        *res;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+
+    text = read_from_file(fd, text);
+    return (res);
 }
 
-void read_from_file();
+char    *read_from_file(int fd, char *text)
+{
+    // read() and add the content from the file to text
+}
 
 void stash_file_buffer();
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,32 @@
 /*   By: fananrak <fananrak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 10:04:47 by fananrak          #+#    #+#             */
-/*   Updated: 2026/03/30 10:04:48 by fananrak         ###   ########.fr       */
+/*   Updated: 2026/03/30 13:43:22 by fananrak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 && !s2)
+		return (NULL);
+	len_s1 = 0;
+	len_s2 = 0;
+	if (s1)
+		len_s1 = ft_strlen(s1);
+	if (s2)
+		len_s2 = ft_strlen(s2);
+	res = ft_calloc((len_s1 + len_s2) + 1, sizeof(char));
+	if (!res)
+		return (0);
+	if (s1)
+		ft_strlcat(res, s1, len_s1 + 1);
+	if (s2)
+		ft_strlcat(res, s2, len_s1 + (len_s2 + 1));
+	return (res);
+}
