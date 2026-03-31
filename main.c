b80258b405514213148d4c7bddfca59a56884efd
Fanantenana Ny Aina Rakotoarison <fananrak@c1r4s8.42antananarivo.mg>
2026-03-31 13:21:54 +0300

    main

--- main.c
+++ main.c
@@ -4,12 +4,14 @@ int main()
 {
     int fd = open("test.txt", O_RDONLY);
 
-    char    *res = get_next_line(fd);
-
-    while (*res)
-    {
-        printf("%s", res);
-    }
+    char    *res1 = get_next_line(fd);
+    printf("print first line: %s", res1);
+    char    *res2 = get_next_line(fd);
+    printf("print second line: %s", res2);
+    char    *res3 = get_next_line(fd);
+    printf("print third line: %s\n", res3);
+/*    char    *res4 = get_next_line(fd);
+    printf("is there last line ?: %s", res4);*/
     close(fd);
     return 0;
 }
